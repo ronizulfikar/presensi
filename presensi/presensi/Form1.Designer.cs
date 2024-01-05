2adc6d5f1349213253e6d825789930fb2cb82018
ronizulfikar <148751221+ronizulfikar@users.noreply.github.com>
2024-01-05 20:45:48 +0700

    Add files via upload

---
+++ presensi/presensi/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace presensi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            txtusername = new TextBox();
+            txtpassword = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(318, 292);
+            button1.Name = "button1";
+            button1.Size = new Size(137, 49);
+            button1.TabIndex = 0;
+            button1.Text = "login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 127);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 1;
+            label1.Text = "username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(37, 166);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 2;
+            label2.Text = "password";
+            // 
+            // txtusername
+            // 
+            txtusername.Location = new Point(142, 132);
+            txtusername.Name = "txtusername";
+            txtusername.Size = new Size(234, 23);
+            txtusername.TabIndex = 3;
+            txtusername.TextChanged += txtusername_TextChanged;
+            // 
+            // txtpassword
+            // 
+            txtpassword.Location = new Point(144, 172);
+            txtpassword.Name = "txtpassword";
+            txtpassword.Size = new Size(232, 23);
+            txtpassword.TabIndex = 4;
+            txtpassword.TextChanged += txtpassword_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(490, 450);
+            Controls.Add(txtpassword);
+            Controls.Add(txtusername);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox txtusername;
+        private TextBox txtpassword;
+    }
+}
